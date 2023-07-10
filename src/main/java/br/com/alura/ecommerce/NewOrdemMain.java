77e40bf25602381092415aa484e48b5f973f498a
marciliodevjava <marciliodevjava@gmail.com>
2023-07-10 11:56:26 -0300

    Update NewOrdemMain

--- src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
@@ -6,20 +6,30 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrdemMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        var produce = new KafkaProducer<String, String>(properties());
-        var value = "123123, 67523 , 789456";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
+        KafkaProducer<String, String> produce = new KafkaProducer<String, String>(properties());
+        String value = "123123, 67523 , 789456";
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
 
-        produce.send(record);
+        produce.send(record, (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviado " + data.topic()
+                    + ":::partition " + data.partition()
+                    + " / offset " + data.offset()
+                    + " / timestamp " + data.timestamp());
+        }).get();
     }
 
     private static Properties properties() {
-        var properties = new Properties();
+        Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
