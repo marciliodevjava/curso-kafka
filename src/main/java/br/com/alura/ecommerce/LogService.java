b2e50907c3fdf633369e4d8b24f0eb1ea50813bf
marciliodevjava <marciliodevjava@gmail.com>
2023-07-12 18:55:46 -0300

    Update LogService

--- src/main/java/br/com/alura/ecommerce/LogService.java
+++ src/main/java/br/com/alura/ecommerce/LogService.java
@@ -36,6 +36,7 @@ public class LogService {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
         properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, LogService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
 
         return properties;
     }
