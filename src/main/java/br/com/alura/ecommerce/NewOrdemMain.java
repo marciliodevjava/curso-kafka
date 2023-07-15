76b9e44dcf01ac28bf5ffd771daa885a8969bc55
marciliodevjava <marciliodevjava@gmail.com>
2023-07-14 21:22:01 -0300

    Update NewOrdemMain

--- src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
@@ -44,6 +44,7 @@ public class NewOrdemMain {
     }
 
     private static Properties properties() {
+
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
