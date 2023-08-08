5ad04e5d3d62779b8997f5584aeaeda3891d9f61
marciliodevjava <marciliodevjava@gmail.com>
2023-08-07 21:13:40 -0300

    Update EmailService

--- src/main/java/br/com/alura/ecommerce/EmailService.java
+++ src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -1,11 +1,13 @@
 package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+
 public class EmailService {
     public static void main(String[] args) {
         var EmailService = new EmailService();
-        var service = new KafkaService(EmailService.class.getSimpleName(),"ECOMMERCE_SEND_EMAIL", EmailService::parse);
-        service.run();
+        try (var service = new KafkaService(EmailService.class.getSimpleName(), "ECOMMERCE_SEND_EMAIL", EmailService::parse)) {
+            service.run();
+        }
     }
 
     private void parse(ConsumerRecord<String, String> record) {
