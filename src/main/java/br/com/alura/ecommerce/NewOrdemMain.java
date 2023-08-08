a7d99b9bd49d728d35d71e98dbb4b0ae14122d02
marciliodevjava <marciliodevjava@gmail.com>
2023-08-07 21:14:16 -0300

    Update NewOrdemMain

--- src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrdemMain.java
@@ -1,55 +1,28 @@
 package br.com.alura.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Properties;
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrdemMain {
+class NewOrdemMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        KafkaProducer<String, String> produce = new KafkaProducer<String, String>(properties());
+        try (var dispatcher = new KafkaDispatcher()) {
 
-        int mensagem1 = new Random().nextInt(1001) + 1000;
-        int mensagem2 = new Random().nextInt(4001) + 1000;
-        int mensagem3 = new Random().nextInt(6001) + 1000;
+            int mensagem1 = new Random().nextInt(1001) + 1000;
+            int mensagem2 = new Random().nextInt(4001) + 1000;
+            int mensagem3 = new Random().nextInt(6001) + 1000;
 
-        String key = UUID.randomUUID().toString();
+            String key = UUID.randomUUID().toString();
+            String value = mensagem1 + ", " + mensagem2 + ", " + mensagem3;
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
 
-        String value = mensagem1 + ", " + mensagem2 + ", " + mensagem3;
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println("sucesso enviado " + data.topic()
-                    + ":::partition " + data.partition()
-                    + " / offset " + data.offset()
-                    + " / timestamp " + data.timestamp());
-        };
-        String email = "Bem-vindo, seu pedido será processado.";
-        ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-
-        produce.send(record, callback).get();
-        produce.send(emailRecord, callback).get();
-    }
-
-    private static Properties properties() {
-
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        return properties;
+            String email = "Bem-vindo, seu pedido será processado.";
+            ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+        }
     }
 }
